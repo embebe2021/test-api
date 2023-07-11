9d46e63c1770eaeae1a82ca41ada2df1f16b9e8c
thangsm <thangsm@magezon.com>
2023-07-11 16:54:06 +0700

    add key+crt

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require("express")
+const https = require("https")
+const fs = require("fs")
+
+const KEY_PATH =
+  "/home/thangsm/ssl/keys/bfd89_723b7_0b73a8d55b1a92a06be144ac0a5e7f4a.key"
+const CERT_PATH =
+  "/home/thangsm/ssl/certs/shopify_magezon_com_bfd89_723b7_1720596676_ae07023a03e4ddb559ad43346a66c6bd.crt"
+
+const httpsOptions = {
+  key: fs.readFileSync(KEY_PATH),
+  cert: fs.readFileSync(CERT_PATH)
+}
 
 const app = express()
 
@@ -8,4 +20,4 @@ app.get("/demo", (req, res) => {
     .json({ success: true, message: "Hello from shopify.magezon.com !" })
 })
 
-app.listen(5111, () => console.log("Hello from shopify.magezon.com !"))
+https.createServer(httpsOptions, app).listen(443)
